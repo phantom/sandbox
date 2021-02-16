891e27f433cded0333c8f6f1d2d424bbfec308f0
Francesco Agosti <francesco.agosti93@gmail.com>
2021-02-16 15:10:54 -0800

    Add signAllTransactions to type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { Connection, PublicKey, Transaction, clusterApiUrl, SystemProgram } from "@solana/web3.js"; 
+import {
+  Connection,
+  PublicKey,
+  Transaction,
+  clusterApiUrl,
+  SystemProgram
+} from "@solana/web3.js";
 import EventEmitter from "eventemitter3";
 import "./styles.css";
 
@@ -8,19 +14,20 @@ interface PhantomProvider extends EventEmitter {
   isConnected: boolean | null;
   autoApprove: boolean | null;
   signTransaction: (transaction: Transaction) => Promise<Transaction>;
-  connect: () => Promise<void>,
-  disconnect: () => Promise<void>,
+  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction>;
+  connect: () => Promise<void>;
+  disconnect: () => Promise<void>;
 }
 
 const getProvider = (): PhantomProvider | undefined => {
-  if ('solana' in window) {
+  if ("solana" in window) {
     const provider = (window as any).solana;
     if (provider.isPhantom) {
       return provider;
     }
-    alert('A provider was found, but not Phantom.');
+    alert("A provider was found, but not Phantom.");
   }
-}
+};
 
 const NETWORK = clusterApiUrl("mainnet-beta");
 
@@ -28,17 +35,17 @@ export default function App() {
   const provider = getProvider();
   const [logs, setLogs] = useState<string[]>([]);
   const addLog = (log: string) => setLogs([...logs, log]);
-  const connection =  new Connection(NETWORK);
+  const connection = new Connection(NETWORK);
   const [, setConnected] = useState<boolean>(false);
   useEffect(() => {
     if (provider) {
-      provider.on('connect', () => {
+      provider.on("connect", () => {
         setConnected(true);
-        addLog('Connected to wallet ' + provider.publicKey?.toBase58());
+        addLog("Connected to wallet " + provider.publicKey?.toBase58());
       });
-      provider.on('disconnect', () => {
+      provider.on("disconnect", () => {
         setConnected(false);
-        addLog('Disconnected from wallet');
+        addLog("Disconnected from wallet");
       });
       return () => {
         provider.disconnect();
@@ -51,29 +58,33 @@ export default function App() {
   const sendTransaction = async () => {
     if (provider.publicKey) {
       try {
-        let transaction = new Transaction().add(SystemProgram.transfer({
-          fromPubkey: provider.publicKey,
-          toPubkey: provider.publicKey,
-          lamports: 100,
-        }));
+        let transaction = new Transaction().add(
+          SystemProgram.transfer({
+            fromPubkey: provider.publicKey,
+            toPubkey: provider.publicKey,
+            lamports: 100
+          })
+        );
         transaction.feePayer = provider.publicKey;
-        addLog('Getting recent blockhash');
+        addLog("Getting recent blockhash");
         (transaction as any).recentBlockhash = (
           await connection.getRecentBlockhash()
         ).blockhash;
-        addLog('Sending signature request to wallet');
+        addLog("Sending signature request to wallet");
         let signed = await provider.signTransaction(transaction);
-        addLog('Got signature, submitting transaction');
+        addLog("Got signature, submitting transaction");
         let signature = await connection.sendRawTransaction(signed.serialize());
-        addLog('Submitted transaction ' + signature + ', awaiting confirmation');
+        addLog(
+          "Submitted transaction " + signature + ", awaiting confirmation"
+        );
         await connection.confirmTransaction(signature);
-        addLog('Transaction ' + signature + ' confirmed');
+        addLog("Transaction " + signature + " confirmed");
       } catch (e) {
         console.warn(e);
-        addLog('Error: ' + e.message);
+        addLog("Error: " + e.message);
       }
     }
-  }
+  };
   return (
     <div className="App">
       <h1>Phantom Sandbox</h1>
@@ -81,8 +92,8 @@ export default function App() {
         {provider && provider.publicKey ? (
           <>
             <div>Wallet address: {provider.publicKey?.toBase58()}.</div>
-            <div>isConnected: {provider.isConnected ? 'true' : 'false'}.</div>
-            <div>autoApprove: {provider.autoApprove ? 'true' : 'false'}. </div>
+            <div>isConnected: {provider.isConnected ? "true" : "false"}.</div>
+            <div>autoApprove: {provider.autoApprove ? "true" : "false"}. </div>
             <button onClick={sendTransaction}>Send Transaction</button>
             <button onClick={() => provider.disconnect()}>Disconnect</button>
           </>
@@ -92,7 +103,9 @@ export default function App() {
         <hr />
         <div className="logs">
           {logs.map((log, i) => (
-            <div className="log" key={i}>{log}</div>
+            <div className="log" key={i}>
+              {log}
+            </div>
           ))}
         </div>
       </main>
