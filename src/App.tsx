59d319ed0a2931d275360efd264b72ea9f59556d
Francesco Agosti <francesco.agosti93@gmail.com>
2021-10-21 18:45:13 -0700

    Add set approval

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import {
   Transaction,
   clusterApiUrl,
   SystemProgram,
+  TransactionInstruction,
 } from "@solana/web3.js";
+import { Token } from "@solana/spl-token";
 import "./styles.css";
 
 type DisplayEncoding = "utf8" | "hex";
@@ -52,12 +54,40 @@ const getProvider = (): PhantomProvider | undefined => {
 };
 
 const NETWORK = clusterApiUrl("mainnet-beta");
+const CONNECTION = new Connection(NETWORK);
+const EXTERNAL_ADDRESS = new PublicKey(
+  "J2XCpwkuvv9XWkPdR7NZyBhajaXA3nt5RGtCnG3JtYiz"
+);
+const TOKEN_PROGRAM_ID = new PublicKey(
+  "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+);
+const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey(
+  "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+);
+const USDC_MINT_ADDRESS = new PublicKey(
+  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+);
+
+async function findAssociatedTokenAddress(
+  walletAddress: PublicKey,
+  tokenMintAddress: PublicKey
+) {
+  return (
+    await PublicKey.findProgramAddress(
+      [
+        walletAddress.toBuffer(),
+        TOKEN_PROGRAM_ID.toBuffer(),
+        tokenMintAddress.toBuffer(),
+      ],
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )
+  )[0];
+}
 
 export default function App() {
   const provider = getProvider();
   const [logs, setLogs] = useState<string[]>([]);
   const addLog = (log: string) => setLogs([...logs, log]);
-  const connection = new Connection(NETWORK);
   const [, setConnected] = useState<boolean>(false);
   useEffect(() => {
     if (provider) {
@@ -80,35 +110,37 @@ export default function App() {
     return <h2>Could not find a provider</h2>;
   }
 
-  const createTransferTransaction = async () => {
+  const createTransaction = async (instructions: TransactionInstruction[]) => {
     if (!provider.publicKey) {
       return;
     }
-    let transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: provider.publicKey,
-        toPubkey: provider.publicKey,
-        lamports: 100,
-      })
-    );
+    let transaction = new Transaction().add(...instructions);
     transaction.feePayer = provider.publicKey;
     addLog("Getting recent blockhash");
     const anyTransaction: any = transaction;
     anyTransaction.recentBlockhash = (
-      await connection.getRecentBlockhash()
+      await CONNECTION.getRecentBlockhash()
     ).blockhash;
     return transaction;
   };
 
-  const sendTransaction = async () => {
-    const transaction = await createTransferTransaction();
+  const createTransferTransaction = async () =>
+    createTransaction([
+      SystemProgram.transfer({
+        fromPubkey: provider.publicKey,
+        toPubkey: provider.publicKey,
+        lamports: 100,
+      }),
+    ]);
+
+  const sendTransaction = async (transaction: Transaction) => {
     if (transaction) {
       try {
         let { signature } = await provider.signAndSendTransaction(transaction);
         addLog(
           "Submitted transaction " + signature + ", awaiting confirmation"
         );
-        await connection.confirmTransaction(signature);
+        await CONNECTION.confirmTransaction(signature);
         addLog("Transaction " + signature + " confirmed");
       } catch (err) {
         console.warn(err);
@@ -116,8 +148,12 @@ export default function App() {
       }
     }
   };
+  const sendTransferInstruction = async () => {
+    const transaction = await createTransferTransaction();
+    sendTransaction(transaction);
+  };
 
-  const signTransaction = async () => {
+  const signTransferTransaction = async () => {
     const transaction = await createTransferTransaction();
     if (transaction) {
       try {
@@ -152,6 +188,20 @@ export default function App() {
       addLog("Signature " + signature);
     }
   };
+
+  const setApproval = async () => {
+    const approveInstruction = Token.createApproveInstruction(
+      TOKEN_PROGRAM_ID,
+      await findAssociatedTokenAddress(provider.publicKey, USDC_MINT_ADDRESS),
+      EXTERNAL_ADDRESS,
+      provider.publicKey,
+      [],
+      1
+    );
+    const transaction = await createTransaction([approveInstruction]);
+    sendTransaction(transaction);
+  };
+
   const signMessage = async (message: string) => {
     const data = new TextEncoder().encode(message);
     try {
@@ -170,14 +220,15 @@ export default function App() {
           <>
             <div>Wallet address: {provider.publicKey?.toBase58()}.</div>
             <div>isConnected: {provider.isConnected ? "true" : "false"}.</div>
-            <button onClick={sendTransaction}>Send Transaction</button>
-            <button onClick={signTransaction}>Sign Transaction</button>
+            <button onClick={sendTransferInstruction}>Send Transaction</button>
+            <button onClick={signTransferTransaction}>Sign Transaction</button>
             <button onClick={() => signMultipleTransactions(false)}>
               Sign All Transactions (multiple){" "}
             </button>
             <button onClick={() => signMultipleTransactions(true)}>
               Sign All Transactions (single){" "}
             </button>
+            <button onClick={setApproval}>Set Approval</button>
             <button
               onClick={() =>
                 signMessage(
